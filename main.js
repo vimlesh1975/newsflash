6e9ba8f829ca417c47f812bd9034c996c76435cd
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2025-05-09 11:57:51 +0530

    nf

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log('object')
+let data2 = [
+    '1 मुख्यमंत्री योगी आदित्यनाथ ने दी मुलायम सिंह को जन्मदिन की बधाई, जानिए क्या कहा ',
+    '2 दिल्ली में प्रदूषण के चलते कंस्ट्रक्शन एक्टिविटी पर लगा प्रतिबंध हटाया गया - NDTV India',
+    '3 चलने नहीं देंगे रामायण एक्सप्रेस, संतों की वेशभूषा में वेटरों पर साधुओं की चेतावनी',
+    '4 मंत्रिमंडल विस्तार से क्या ख़त्म हो जाएगी अशोक गहलोत और सचिन पायलट गुट की कलह?'
+];
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    let i = 0
+    updatestring('id_310', data2[i]);
+    
+    setInterval(() => {
+        sheet.sequence.position = 0;
+        updatestring('id_310', data2[i]);
+        sheet.sequence.play({ range: [0, 10] });
+        i = i + 1;
+        if (i >= data2.length) {
+            i = 0;
+    
+        }
+    }, 7000);
+})
